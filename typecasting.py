bc549b6b5d13e4fbf9917cffd845b8bd2b5e9391
James Githua <jamesgithua789@gmail.com>
2026-09-21 10:31:45 +0300

    TypeCasting Program with User Input

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+#Calculating Area of Rectangle
+
+#length = float(input("Enter the length of rectangle: "))
+#width = float(input("Enter the breadth of rectangle: "))
+
+#area = length * width
+#print("Area of rectangle is: ", area)
+
+#Shopping Cart Program
+
+item = input("What would you like to Purchase?: ")
+price = float(input("Enter the price of the item: "))
+quantity = int(input("Enter the quantity of the item: "))
+total = price * quantity
+print(f'The total cost for {quantity} {item}(s) is: ${total}')
