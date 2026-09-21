3a8f91b79187c10e48b5f841528f90c234c083fa
James Githua <jamesgithua789@gmail.com>
2026-09-21 11:29:35 +0300

    Basic If statement in python

---
+++ if_statement.py
@@ -0,0 +1,16 @@
+isOnline = False
+
+if isOnline:
+	print("user is online")
+else:
+	print("user is not online")
+
+
+age = 20
+if age > 100 or age < 0:
+    print("user is not eligible to vote")
+elif age >= 18:
+    print("user is eligible to vote")
+
+else:
+    print("user is not eligible to vote") 
